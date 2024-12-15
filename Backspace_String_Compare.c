8319161400d02d4edf73de8f7e5f43016fd3395d
Hoàng Khang <khangrubikcuber@gmail.com>
2024-12-15 15:54:29 +0700

    Update Backspace_String_Compare.c

--- Backspace_String_Compare.c
+++ Backspace_String_Compare.c
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-// Example 1:
-// Input: s = "ab#c", t = "ad#c"
-// Output: true
-// Explanation: Both s and t become "ac".
-
-// Example 2:
-// Input: s = "ab##", t = "c#d#"
-// Output: true
-// Explanation: Both s and t become "".
-
-// Example 3:
-// Input: s = "a#c", t = "b"
-// Output: false
-// Explanation: s becomes "c" while t becomes "b".
+//https://leetcode.com/problems/backspace-string-compare/
 #include <stdio.h>
 #include <string.h>
 void checkS(char s1[]);
